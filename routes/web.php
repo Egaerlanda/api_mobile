0cbe23bb0f3d7aba39274fd9d90c0cf4102a5c53
Egaerlanda <12210463@bsi.ac.id>
2023-05-14 20:09:00 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PenggunaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::namespace('App\Http\Controllers')->middleware('cek.apikey')->group(function(){
+
+    Route::get('logiin', 'PenggunaController@login');
+    Route::delete('login', 'PenggunaController@logout');
+
+    Route::group(['prefix'=>'Pengguna', 'middleware' => ['cek.user']], function(){
+        Route::patch('/', 'PenggunaController@update');
+        Route::post('/photo', 'PenggunaController@simpan_photo');
+        Route::get('/photo', 'PenggunaController@photo');
+    });
+
+    Route::prefix('warga')->middleware(['cek.user'])->group(function(){
+        Route::post('/', 'WargaController@store');
+        Route::patch('/{w}', 'WargaController@update');
+        Route::delete('/{w}', 'WargaController@delete');
+        Route::get('/{w}', 'WargaController@show');
+    });
 });
